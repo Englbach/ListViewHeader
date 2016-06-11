521291cfe2f5da88b011ce7a33355690746db1a1
Englbach <lethien.hoang@yahoo.com>
2016-06-12 00:46:36 +0700

    175

---
+++ ListViewHeader/MainActivity.cs
@@ -0,0 +1,165 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System.Collections.Generic;
+using Android.Views;
+using System;
+
+namespace ListViewHeader
+{
+	[Activity(Label = "ListViewHeader", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+
+		public static int TYPE_ITEM = 0;
+		public static int TYPE_SEPORATOR=1;
+
+		List<IMenuItemsType> item = new List<IMenuItemsType>();
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			item.Add(new MenuHeaderItem("GENERAL SETTINGS"));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display",Resource.Drawable.Delete15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.DoubleTap15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Settings15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.ShowProperty15));
+			item.Add(new MenuHeaderItem("NOTIFICATIONS"));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Settings15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Phone15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.EditUserMale15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.DoubleTap15));
+			item.Add(new MenuHeaderItem("GENERAL SETTINGS"));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Delete15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.DoubleTap15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Settings15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.ShowProperty15));
+			item.Add(new MenuHeaderItem("NOTIFICATIONS"));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Settings15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.Phone15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.EditUserMale15));
+			item.Add(new MenuContentItem("Display Settings", "Settings about screen display", Resource.Drawable.DoubleTap15));
+
+			ListView lst = FindViewById<ListView>(Resource.Id.lstview);
+			lst.Adapter = new ListViewAdapter(this, item);
+			         
+		}
+
+		public interface IMenuItemsType
+		{
+			int GetMenuItemsType();
+		}
+
+		public class MenuHeaderItem : IMenuItemsType
+		{
+			public string HeaderText { get; set; }
+
+			public int GetMenuItemsType()
+			{
+				return TYPE_ITEM;
+			}
+
+			public MenuHeaderItem(string _headerText)
+			{
+				HeaderText = _headerText;
+			}
+		}
+
+		public class MenuContentItem : IMenuItemsType
+		{
+			public string Title { get; set; }
+			public string SubTitle { get; set; }
+			public int IconImage { get; set; }
+
+			public int GetMenuItemsType()
+			{
+				return TYPE_SEPORATOR;
+			}
+
+			public MenuContentItem(string _title, string _subtitle, int _iconImage)
+			{
+				Title = _title;
+				SubTitle = _subtitle;
+				IconImage = _iconImage;
+			}
+		}
+
+
+
+		public class ListViewAdapter : ArrayAdapter<IMenuItemsType>
+		{
+			private Context context;
+			private List<IMenuItemsType> items;
+			private LayoutInflater inflater;
+
+			public ListViewAdapter(Context context, List<IMenuItemsType> items) : base(context,0,items)
+			{
+				this.context = context;
+				this.items = items;
+				this.inflater = (LayoutInflater)this.context.GetSystemService(Context.LayoutInflaterService);
+			}
+
+
+			public override int Count
+			{
+				get
+				{
+					//throw new System.NotImplementedException();
+					return items.Count;
+				}
+			}
+
+			public override long GetItemId(int position)
+			{
+				//throw new System.NotImplementedException();
+				return position;
+			}
+
+			public override View GetView(int position, View convertView, ViewGroup parent)
+			{
+				//throw new System.NotImplementedException();
+				View view = convertView;
+				try
+				{
+					IMenuItemsType item = items[position];
+					if (item.GetMenuItemsType() == TYPE_ITEM)
+					{
+						MenuHeaderItem _headerItem = (MenuHeaderItem)item;
+						view = inflater.Inflate(Resource.Layout.ListViewHeaderItem, null);
+						// user dont click header item
+						view.Clickable = false;
+
+						var headerName = view.FindViewById<TextView>(Resource.Id.txtHeader);
+						headerName.Text = _headerItem.HeaderText;
+
+					}
+					else if (item.GetMenuItemsType() == TYPE_SEPORATOR)
+					{
+						MenuContentItem _contentItem = (MenuContentItem)item;
+						view = inflater.Inflate(Resource.Layout.ListViewContentItem, null);
+
+						var _title = view.FindViewById<TextView>(Resource.Id.txtTitle);
+						var _imgIcon = view.FindViewById<ImageView>(Resource.Id.imgIcon);
+						var _subTitle = view.FindViewById<TextView>(Resource.Id.txtSubTitle);
+
+						_title.Text = _contentItem.Title;
+						_imgIcon.SetBackgroundResource(_contentItem.IconImage);
+						_subTitle.Text = _contentItem.SubTitle;
+					}
+				}
+				catch (Exception ex)
+				{
+					Toast.MakeText(context, ex.Message, ToastLength.Long);
+				}
+				return view;
+			}
+		}
+	}
+}
+
+
---
+++ ListViewHeader/obj/Debug/android/src/md5f81944d3e10d1312f75d88077ba93d98/MainActivity_ListViewAdapter.java
@@ -0,0 +1,105 @@
+package md5f81944d3e10d1312f75d88077ba93d98;
+
+
+public class MainActivity_ListViewAdapter
+	extends android.widget.ArrayAdapter
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_getCount:()I:GetGetCountHandler\n" +
+			"n_getItemId:(I)J:GetGetItemId_IHandler\n" +
+			"n_getView:(ILandroid/view/View;Landroid/view/ViewGroup;)Landroid/view/View;:GetGetView_ILandroid_view_View_Landroid_view_ViewGroup_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity_ListViewAdapter.class, __md_methods);
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1) throws java.lang.Throwable
+	{
+		super (p0, p1);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1, int p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1, java.lang.Object[] p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:T[], Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1, int p2, java.lang.Object[] p3) throws java.lang.Throwable
+	{
+		super (p0, p1, p2, p3);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:T[], Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0, p1, p2, p3 });
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1, java.util.List p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Collections.Generic.IList`1<T>, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public MainActivity_ListViewAdapter (android.content.Context p0, int p1, int p2, java.util.List p3) throws java.lang.Throwable
+	{
+		super (p0, p1, p2, p3);
+		if (getClass () == MainActivity_ListViewAdapter.class)
+			mono.android.TypeManager.Activate ("ListViewHeader.MainActivity+ListViewAdapter, ListViewHeader, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Collections.Generic.IList`1<T>, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2, p3 });
+	}
+
+
+	public int getCount ()
+	{
+		return n_getCount ();
+	}
+
+	private native int n_getCount ();
+
+
+	public long getItemId (int p0)
+	{
+		return n_getItemId (p0);
+	}
+
+	private native long n_getItemId (int p0);
+
+
+	public android.view.View getView (int p0, android.view.View p1, android.view.ViewGroup p2)
+	{
+		return n_getView (p0, p1, p2);
+	}
+
+	private native android.view.View n_getView (int p0, android.view.View p1, android.view.ViewGroup p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
